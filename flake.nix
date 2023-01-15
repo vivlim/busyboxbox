a1881ae35f1569705f1d900ba54af9d774bbd831
Viv Lim <vivlim@pm.me>
2023-01-15 01:01:21 -0800

    initial commit

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "busybox shell in an appimage";
+
+  # nix build .#defaultPackage.powerpc64le-linux
+
+  inputs = {
+    # need nixpkgs including https://github.com/NixOS/nixpkgs/pull/199978
+    nixpkgs.url      = "github:NixOS/nixpkgs/release-22.11";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    (flake-utils.lib.eachSystem (flake-utils.lib.defaultSystems ++ ["powerpc64le-linux"]) (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      with pkgs;
+      {
+        defaultPackage = let 
+        busybox-bin = pkgs.stdenv.mkDerivation {
+          name = "busybox-bin";
+          phases = "installPhase";
+          installPhase = ''
+            mkdir $out
+            ${pkgs.busybox}/bin/busybox --install -s $out
+          '';
+        };
+        in writeShellScriptBin "entry" ''
+        export PATH=${busybox-bin}:${nix}/bin:$PATH
+        echo "you're in busyboxbox now, friend."
+        uname -a
+        ${busybox-bin}/sh
+        '';
+      }
+    ));
+}
+
